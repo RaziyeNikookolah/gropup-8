272d112535a9fdf48d85e1f09d11945971a988b2
kiana alamy <as.faraso.97@gmail.com>
2023-05-19 15:39:28 +0430

    kiana_q1

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from typing import List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+# create a list of dictionary objects to simulate a real system
+items = [
+    {"id": 1, "name": "kiana", "description": "description1"},
+    {"id": 2, "name": "katayoon", "description": "description2"},
+    {"id": 3, "name": "gity", "description": "description3"}
+]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# GET /items/{item_id}: Return an item given an id
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    for item in items:
+        if item["id"] == item_id:
+            return item
+    # If the item is not found, return an HTTP 404 Not Found response
+    return {"error": "Item not found."}
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# POST /items: Create a new item in the system
+@app.post("/items")
+def create_item(item: dict):
+    items.append(item)
+    return {"message": "Item created successfully."}
