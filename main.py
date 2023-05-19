48fd56f95a05e3e265d38cf874a0677af945bf45
mohammad <mohammadhp858@gmail.com>
2023-05-19 11:30:25 +0430

    add post method

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from products import product_list
+
+
+class Products(BaseModel):
+    name: str
+    price: int
+    category: str
+
 
 app = FastAPI()
 
 
-app.get('/product/{id}')
-def get_item(id: int):
-    product_list.get(id)
+@app.get("/product", response_model=dict[int, Products])
+def all_item():
+    return product_list.items()
 
 
-app.post('/product/')
-def add_item():
-    ...
+@app.get("/product/{id}")
+def get_item(id: int) -> dict:
+    item = product_list.get(id)
+    if not item:
+        raise HTTPException(status_code=404, detail="product doesn't exists ")
+    return item
+
+
+@app.post("/product/")
+def add_item(product: Products):
+    last_id = max(product_list.keys())
+    product_list[last_id+1] = product
+    
--- products.py
+++ products.py
@@ -1,8 +1,18 @@
 product_list = {
     1: {
-        'name': 'notebook',
+        'name': 'apple',
         'price' : 30,
-        'category' : ''
-
+        'category' : 'fruits'
+    },
+    2: {
+        'name' : 'cherry',
+        'price' : 45,
+        'category' : 'fruits'
+    }, 
+    3: {
+        'name': 'perfume',
+        'price': 95,
+        'category': 'accessories'
     }
 }
+
