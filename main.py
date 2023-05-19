85c261c84aa2fbbe68c339c73840faffe79733fe
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-19 10:41:56 +0000

    fastapi cw completed

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ class Item(BaseModel):
 
 
 items = [
-    {"id": 1, "name": "abbas"},
-    {"id": 2, "name": "mohammad"},
-    {"id": 3, "name": "vahid"},
+    {"id": 1, "name": "sara"},
+    {"id": 2, "name": "maryam"},
+    {"id": 3, "name": "bahar"},
 ]
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    for item in items:
-        if item["id"] == item_id:
-            return item
-    raise HTTPException(status_code=404, detail="Item not found")
+    found_item = list(filter(lambda item: item["id"] == item_id, items))
+    if found_item:
+        return found_item[0]
+    raise HTTPException(status_code=HTTPException.HTTP_, detail="Item not found")
 
 
 @app.post("/items")
 def create_item(item: Item):
     items.append(item.dict())
-    return {"message": "Item created"}
+    return {"message": "Added.."}
